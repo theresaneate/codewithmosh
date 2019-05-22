02f1da4d093e62cf43ffab662fb912d92e99397d
Theresa Neate <theresaneate@gmail.com>
2019-05-22 20:52:57 +1000

    beginner variable exercises

---
+++ exercises_variables.py
@@ -0,0 +1,22 @@
+# https://pythonbasics.org/exercises/
+# Variables
+
+
+# Make a program that displays several numbers.
+var1 = str(1)
+var2 = str(2)
+var3 = str(3)
+
+print("Var 1 is: " + var1)
+print("Var 2 is: " + var2)
+print("Var 3 is: " + var3)
+
+# Make a program that solves and shows the summation of 64 + 32.
+sum = 64 + 32
+print("Summation 1 is: " + str(sum))
+
+# Do the same as in 2, but make it sum x + y.
+x = 64
+y = 32
+sum = x + y
+print("Summation 2 is: " + str(sum))
