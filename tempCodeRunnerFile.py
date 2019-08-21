679ba2389b94fbd99b92c62d6e01cf9f90e8e448
Theresa Neate <theresa.neate@rea-group.com>
2019-08-21 15:35:28 +1000

    adding edited code runner

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,13 +1,2 @@
-
-# # third exercise
-# print("third - looping over numbers e.g. multiplying them:")
-
-
-# def multiply3(*list):
-#     total = 1
-#     for number in list:
-#         total *= number
-#     return total
-
-
-# print(multiply3(1, 2, 3, 4))
+    if (myinput % 3 == 0.0 and input % 5 == 0.0):
+        print("FizzBuzz")
