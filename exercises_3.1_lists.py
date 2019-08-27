9bf4a3dc357cbff91fedd3a6ed9f869cacb185e1
Theresa Neate <theresa.neate@rea-group.com>
2019-08-27 20:45:53 +1000

    Filename corrected and added

---
+++ exercises_3.1_lists.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# https://codewithmosh.com/courses/423295/lectures/6781740
+
+letters = ["a", "b", "c"]
+numbers = list(range(5))
+
+print("Matrix:")
+matrix = [[0, 1, 2], [2, 3, 4]]
+print(matrix)
+print(matrix[0])
+print(matrix[0])
+print(matrix[1])
+
+print("Zeroes:")
+zeros = [0] * 10
+print(zeros)
+
+print("Combined:")
+combined = zeros + letters
+print(combined)
+print("Length of Combined = " + str(len(combined)))
+
+print("Mystring:")
+mystring = list("Hello World")
+print(mystring)
+print(type(mystring))
