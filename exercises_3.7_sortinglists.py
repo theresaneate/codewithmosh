005629d6e5237e542b8f3e40a93e1ca40dfefa79
Theresa Neate <theresaneate@gmail.com>
2019-09-05 22:20:59 +1000

    sorting lists

--- exercises_3.7_sortinglists.py
+++ exercises_3.7_sortinglists.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python3
 # https://codewithmosh.com/courses/423295/
 
-print("\nnumbers.sorted:")
-numbers = [4, 1, 41, 4, 99, 22]
-print(numbers)
-numbers.sort()
-print(numbers)
+# print("\nnumbers.sorted:")
+# numbers = [4, 1, 41, 4, 99, 22]
+# print(numbers)
+# numbers.sort()
+# print(numbers)
 
-print("\nreinitialise and use sorted():")
-numbers = [4, 1, 41, 4, 99, 22]
-print(numbers)
-print(sorted(numbers))
-print(sorted(numbers, reverse=True))
+# print("\nreinitialise and use sorted():")
+# numbers = [4, 1, 41, 4, 99, 22]
+# print(numbers)
+# print(sorted(numbers))
+# print(sorted(numbers, reverse=True))
 
 
 items = [
-
-
-
-
+    ("Product", 10)
+    ("Product", 9)
+    ("Product", 12)
 ]
+
+def sort_item(item):
+    item[1]
+
+items.sort(key=sort_item)
+print(items)
