91352119b8039bc3181be5b54be764a2f0555387
Theresa Neate <theresa.neate@rea-group.com>
2019-09-09 11:14:16 +1000

    3.7 commit

--- exercises_3.7_sortinglists.py
+++ exercises_3.7_sortinglists.py
@@ -1,27 +1,30 @@
 #!/usr/bin/env python3
 # https://codewithmosh.com/courses/423295/
 
-# print("\nnumbers.sorted:")
-# numbers = [4, 1, 41, 4, 99, 22]
-# print(numbers)
-# numbers.sort()
-# print(numbers)
+print("\nnumbers.sorted:")
+numbers = [4, 1, 41, 4, 99, 22]
+print(numbers)
+numbers.sort()
+print(numbers)
 
-# print("\nreinitialise and use sorted():")
-# numbers = [4, 1, 41, 4, 99, 22]
-# print(numbers)
-# print(sorted(numbers))
-# print(sorted(numbers, reverse=True))
+print("\nreinitialise and use sorted():")
+numbers = [4, 1, 41, 4, 99, 22]
+print(numbers)
+print(sorted(numbers))
+print(sorted(numbers, reverse=True))
 
 
+print("\nsort a list of tuples:")
 items = [
-    ("Product", 10)
-    ("Product", 9)
-    ("Product", 12)
+    ("Product", 10),
+    ("Product", 9),
+    ("Product", 12),
 ]
 
+
 def sort_item(item):
-    item[1]
+    return item[1]
+
 
 items.sort(key=sort_item)
 print(items)
