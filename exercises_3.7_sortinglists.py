b9abbb647ffb45e8d23c9e398f9317e7db40bd60
Theresa Neate <theresa.neate@rea-group.com>
2019-09-18 15:44:22 +1000

    Getting ready for homework of week 19 Sept 2019

--- exercises_3.7_sortinglists.py
+++ exercises_3.7_sortinglists.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # https://codewithmosh.com/courses/423295/
 
-print("\nnumbers.sorted:")
+print("\nnumbers.sort:")
 numbers = [4, 1, 41, 4, 99, 22]
 print(numbers)
-numbers.sort()
+print(numbers.sort())
 print(numbers)
 
 print("\nreinitialise and use sorted():")
---
+++ exercises_3.8_lambdas.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python3
+# https://codewithmosh.com/courses/423295/
