9bcdda9af2ad5e99fea890b9518557a9e14752a8
Theresa Neate <theresa.neate@rea-group.com>
2019-09-18 21:26:06 +1000

    beginning of lambdas

--- exercises_3.12_zipfunction.py
+++ exercises_3.12_zipfunction.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
 # https://codewithmosh.com/courses/423295/
+
+list1 = [1, 2, 3]
+list2 = [10, 20, 30]
+
+# we want
+# [(1, 10), (2, 20), (3, 30)]
+
+print(list(zip(list1, list2)))
+print(list(zip("abc", list1, list2)))
--- exercises_3.9_maps.py
+++ exercises_3.9_maps.py
@@ -1,2 +1,28 @@
 #!/usr/bin/env python3
 # https://codewithmosh.com/courses/423295/
+
+items = [
+    ("Product1", 10),
+    ("Product2", 9),
+    ("Product3", 12),
+
+]
+
+print("The inelegant way first:")
+prices = []
+for item in items:
+    prices.append(item[1])
+
+
+print(prices)
+
+
+print("\nThe more elegant map way:")
+
+x = map(lambda item: item[1], items)
+for item in x:
+    print(item)
+
+print("\nThe list way:")
+prices = list(map(lambda item: item[1], items))
+print(prices)
