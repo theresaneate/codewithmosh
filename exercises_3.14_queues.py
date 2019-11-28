653d4e3a4ff6ef2acfbd21905c0f0fac4bc11218
Theresa Neate <theresa.neate@rea-group.com>
2019-11-28 12:11:30 +1100

    learning queues

---
+++ exercises_3.14_queues.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# https://codewithmosh.com/courses/423295/
+
+from collections import deque
+
+# queues are lists but require you to import collections.deque class
+# and to convert a list with deque
+
+
+myqueue = deque(["Theresa", "Ben"])
+
+print("\nAdding a 1 using append")
+myqueue.append(1)
+print(myqueue)
+
+print("\nPopleft")
+myqueue.popleft()
+print(myqueue)
+
+print("\nPopleft")
+myqueue.popleft()
+print(myqueue)
+
+print("\nPop")
+myqueue.pop
+print(myqueue)
+
+print("\nCheck if empty with 'if not 'myqueue':")
+if not myqueue:
+    print("empty")
+else:
+    print("not empty")
