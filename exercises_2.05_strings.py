4951c84e856a32b5ab3e823b499faed0b05e02a4
Theresa Neate <theresa.neate@rea-group.com>
2020-01-03 15:07:20 +1100

    renamed files for clarity on numbering

--- exercises_2.05_strings.py
+++ exercises_2.05_strings.py
@@ -7,20 +7,33 @@ import os
 os.system("clear")
 
 course = "Python Programming"
+
+print("\nLength of string using len()")
 print(len(course))
+
+print("\nMemory location of string using id()")
 print(id(course))
+
+print("\nFirst character [0]")
 print(course[0])
 
-# negative index, not seen in other program
+print("\nnegative index, not seen in other program -1, then -2")
 print(course[-1])
-
-
 print(course[-2])
+
+print("\nslice a string [0:3]")
 print(course[0:3])
+
+print("\neverything up to index 3")
 print(course[:3])
+
+print("\nstart at 0 and don't provide end")
 print(course[0:])
+
+print("\nprint whole string")
 print(course[:])
 
+print("\nmemory is allocated individually to string and chars (immutable)")
 print(id(course))
 print(id(course[0]))
 print(id(course[:]))
--- exercises_2.06_escapeseq.py
+++ exercises_2.06_escapeseq.py
@@ -1,11 +1,22 @@
+#!/usr/bin/env python3
+# https://codewithmosh.com/courses/423295/lectures/6781752
+
+import os
+
+# to clear the screen!
+os.system("clear")
+
+
 message = """
 Python
 Programming
 """
 print(message)
 
-message2 = "\nMy newline\n"
-print(message2)
+print("double quote \" ")
 
-message3 = "Single quote \' "
-print(message3)
+print("\nMy newline\n")
+
+print("Single quote \' ")
+
+print("Backslash \\ ")
